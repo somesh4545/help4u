31ed97923c9cecd9d55367a3c960d6b325abdd9f
Somesh <someshsomani457@gmail.com>
2022-01-09 22:47:24 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,183 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useMemo, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: "auto",
+  height: 200,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
+
+function StyledDropzone(props) {
+  const [files, setFiles] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+    acceptedFiles,
+    open,
+  } = useDropzone({
+    accept: "image/*",
+    noClick: true,
+    noKeyboard: true,
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject]
+  );
+
+  const thumbs = files.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img src={file.preview} style={img} />
+      </div>
+    </div>
+  ));
+
+  useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
+
+  const filepath = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const send = () => {
+    console.log(files[0]);
+    var bodyFormData = new FormData();
+    setLoading(true);
+    setResult(null);
+    bodyFormData.append("file", files[0]);
+    axios({
+      method: "post",
+      url: "https://somesh-ocr.herokuapp.com/upload",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        console.log(response.data.string);
+        setResult(response.data.string);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setLoading(false);
+        setResult(null);
+      });
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(result);
+  };
+
+  return (
+    <div className="container">
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here</p>
+        <ul>{filepath}</ul>
+        <button type="button" onClick={open}>
+          Open File Dialog
+        </button>
+      </div>
+      <div className="resultBox">
+        <button className="btn" onClick={send}>
+          Read image
+        </button>
+        {result != null ? (
+          <button className="btn" onClick={copyToClipboard}>
+            copy
+          </button>
+        ) : null}
+      </div>
+
+      {/* <aside>
+        <h4>Files</h4>
+      </aside> */}
+    </div>
+  );
+}
+
+ReactDOM.render(<StyledDropzone />, document.getElementById("root"));
